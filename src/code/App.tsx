122d68ca20fc182b03fde36a7f8076d0d4603010
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2021-12-25 11:06:56 +0100

    first commit

---
+++ src/code/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello there</h1>
+    </div>
+  );
+}
+
+export default App;
