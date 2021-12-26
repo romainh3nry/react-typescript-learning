1dea0485c2b21b57e40bd4138e060ece48a21158
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2021-12-26 10:48:00 +0100

    useContext hook

--- src/code/App.tsx
+++ src/code/App.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import '../css/App.css';
-import { Button } from './Button';
-import { Container } from './Container';
-import { Greet } from './Greet';
-import { Heading } from './Heading';
-import { Input } from './Input';
-import { Oscar } from './Oscar';
-import { LoggedIn } from './state/LogginIn';
-import { Status } from './Status';
+import { ThemeContextProvider } from './context/ThemeContext';
+import { Box } from './context/Box';
 
 const App = () => {
 
   return (
     <div className="App">
-      <Status status='loading' />
-      <Heading>Placeholder Text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Tom Hanks</Heading>
-      </Oscar>
-      <Greet name="Henry Romain" isLoggedIn={true} />
-      <Input value='' />
-      <Button handleClick={e => console.log('clicked', e)} />
-      <Container styles={{border: "1px solid red"}} />
-      <LoggedIn />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
